22d58397c3f26bd04f206e30c58d3e81fd95a44e
Andy <thingswithhandles@gmail.com>
2015-09-03 16:29:05 -0500

    More styling

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,20 @@
 var gulp           = require('gulp');
 var sass           = require('gulp-sass'),
 	sourcemaps     = require('gulp-sourcemaps'),
+	concat         = require('gulp-concat-css'),
 	autoprefixer   = require('gulp-autoprefixer');
 
 gulp.task('styles', function() {
 	gulp.src('sass/**/*.scss')
 		.pipe(sourcemaps.init())
-			.pipe(sass({outputStyle: 'compressed'})
+			.pipe(sass({outputStyle: 'nested'})
 			      	  .on('error', sass.logError)
 			      )
 		.pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
+        .pipe(concat('main.css'))
 		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest('./css'));
 });
