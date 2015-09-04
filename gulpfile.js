a739e490061dc8fd29a8711d4f3b2b354a35f400
Andy Rose <thingswithhandles@gmail.com>
2015-09-04 17:14:22 -0500

    Minify CSS using Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp           = require('gulp');
 var sass           = require('gulp-sass'),
 	sourcemaps     = require('gulp-sourcemaps'),
 	concat         = require('gulp-concat-css'),
+    minifyCSS      = require('gulp-minify-css'),
 	autoprefixer   = require('gulp-autoprefixer');
 var imagemin       = require('gulp-imagemin');
 var pngquant       = require('imagemin-pngquant');
@@ -9,14 +10,15 @@ var pngquant       = require('imagemin-pngquant');
 gulp.task('styles', function() {
 	gulp.src('sass/**/*.scss')
 		.pipe(sourcemaps.init())
-			.pipe(sass({outputStyle: 'nested'})
-			      	  .on('error', sass.logError)
-			      )
+		.pipe(sass({outputStyle: 'nested'})
+		      	  .on('error', sass.logError)
+		      )
 		.pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
         .pipe(concat('main.css'))
+        .pipe(minifyCSS())
 		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest('./css'));
 });
